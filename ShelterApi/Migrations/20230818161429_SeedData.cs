7cf1e8b840cc0aca861b6d019dd957d12acd9d3c
Jason E. Church <elijahchurch@gmail.com>
2023-08-18 09:16:21 -0700

    Update Cat and Dog Age to string type. Seed data into database and update it

---
+++ ShelterApi/Migrations/20230818161429_SeedData.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShelterApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "Dogs",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Age",
+                table: "Cats",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Gender", "HealthSocialNeeds", "Name", "Personality" },
+                values: new object[,]
+                {
+                    { 1, "3m", "Female", "Need time to adjust to new environment.", "Dinky", "Sensitive, shy, worried around new people and environements" },
+                    { 2, "2y 3m", "Male", "None.", "Chester", "Affectionate" },
+                    { 3, "8y", "Male", "Deaf. Should be in a home with no other pets.", "Trooper", "Loud and friendly!" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Gender", "HealthSocialNeeds", "Name", "Personality" },
+                values: new object[,]
+                {
+                    { 1, "1y 8m", "Male", "Would do best in an environment with older children.", "Houdini", "High Energy and Goofy!" },
+                    { 2, "2y 4m", "Male", "While not needed, would do better in a home that has another dog for him to play with.", "Jelly Bean", "Very Social" },
+                    { 3, "7y", "Female", "Blind. Would do better in an environment with no children", "Farrah", "Shy and Nervous" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "EmailAddress", "Password", "UserName" },
+                values: new object[] { 1, "TestUser@gmail.com", "Test", "TestUser" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Dogs",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "Cats",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- ShelterApi/Models/ShelterApiContext.cs
+++ ShelterApi/Models/ShelterApiContext.cs
@@ -4,12 +4,31 @@ namespace ShelterApi.Models
 {
     public class ShelterApiContext : DbContext
     {
-        public DbSet<Cat> Cats { get; set;}
-        public DbSet<Dog> Dogs { get; set;}
-        public DbSet<User> Users { get; set;}
+        public DbSet<Cat> Cats { get; set; }
+        public DbSet<Dog> Dogs { get; set; }
+        public DbSet<User> Users { get; set; }
         public ShelterApiContext(DbContextOptions<ShelterApiContext> options) : base(options)
         {
-            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Dog>()
+                .HasData(
+                    new Dog { DogId = 1, Name = "Houdini", Gender = "Male", Age = "1y 8m", Personality = "High Energy and Goofy!", HealthSocialNeeds = "Would do best in an environment with older children."},
+                    new Dog { DogId = 2, Name = "Jelly Bean", Gender = "Male", Age = "2y 4m", Personality = "Very Social", HealthSocialNeeds = "While not needed, would do better in a home that has another dog for him to play with."},
+                    new Dog { DogId = 3, Name = "Farrah", Gender = "Female", Age = "7y", Personality = "Shy and Nervous", HealthSocialNeeds = "Blind. Would do better in an environment with no children"}      
+                );
+            builder.Entity<Cat>()
+                .HasData(
+                    new Cat { CatId = 1, Name = "Dinky", Gender = "Female" , Age = "3m", Personality = "Sensitive, shy, worried around new people and environements" , HealthSocialNeeds = "Need time to adjust to new environment."},
+                    new Cat { CatId = 2, Name = "Chester", Gender = "Male" , Age =  "2y 3m", Personality = "Affectionate", HealthSocialNeeds = "None." },
+                    new Cat { CatId = 3, Name = "Trooper", Gender = "Male", Age = "8y", Personality = "Loud and friendly!" , HealthSocialNeeds = "Deaf. Should be in a home with no other pets."}
+                );
+            builder.Entity<User>()
+                .HasData(
+                    new User { UserId = 1, UserName = "TestUser", Password = "Test", EmailAddress = "TestUser@gmail.com"}       
+                );
         }
     }
 }
